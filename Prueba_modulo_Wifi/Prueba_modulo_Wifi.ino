a68b09205827f6604419bc888b56991d3ee80019
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2018-05-17 07:31:26 -0500

    Add files via upload

--- Prueba_modulo_Wifi/Prueba_modulo_Wifi.ino
+++ Prueba_modulo_Wifi/Prueba_modulo_Wifi.ino
@@ -31,17 +31,50 @@ void configWIFI(){
 
 void runWIFI(){
   long int time = millis(); // medir el tiempo actual para verificar timeout
-  BT1.print("AT+CIFSR\r\n"); // enviar el comando al ESP8266
+  //BT1.print("AT+CIFSR\r\n"); // enviar el comando al ESP8266
+  BT1.print("AT+CIPSTA?\r\n");
   int contador = 0;
   String ip;
   while( (time+1000) > millis()) {//mientras no haya timeout
     while(BT1.available()) {//mientras haya datos por leer
       // Leer los datos disponibles
       char c = BT1.read(); // leer el siguiente caracter
-      ip = Serial.print(c)+ip;  
+      Serial.print(c);
+      ip = ip+c;  
     }
   }
-  Serial.println(ip.substring(ip.indexOf("+CIFSR:STAIP,"), ip.indexOf("+CIFSR:STAMAC")));  
+
+  int pos1=0, pos2=0;
+  Serial.println("Imprimiendo IP :"+ip+ "******");
+  Serial.print("Indice 1:");
+  pos1=ip.indexOf("gateway");
+  pos1+=9;
+  Serial.println(pos1);
+  Serial.print("Indice 2:");
+  pos2=ip.indexOf("netmask");
+  pos2-=11;
+  Serial.println(pos2);
+  String gateway = ip.substring(pos1,pos2);
+  Serial.println("gateway ACTUAL:" + gateway);
+  //Serial.println(ip.substring(ip.indexOf("+CIFSR:STAIP,"), ip.indexOf("+CIFSR:STAMAC")));
+  Serial.println("FIN Imprimiendo IP");
+
+//Armamos el encabezado de la peticion http
+  String peticionHTTP= "GET /?bandera=0&id=1&latitud=1.0&longitud=2.0&lleno=0";
+  peticionHTTP=peticionHTTP+String(variable1)+"&b="+String(variable2)+" HTTP/1.1\r\n";
+  peticionHTTP=peticionHTTP+"Host: "+gateway+"\r\n\r\n";
+
+  //Enviamos el tamaño en caracteres de la peticion http:  
+  SerialESP8266.print("AT+CIPSEND=");
+  SerialESP8266.println(peticionHTTP.length());
+
+  //esperamos a ">" para enviar la petcion  http
+  if(SerialESP8266.find(">")) // ">" indica que podemos enviar la peticion http
+  {
+    Serial.println("Enviando HTTP . . .");
+    SerialESP8266.println(peticionHTTP);
+  }
+  
 }
 
 void setup(){
